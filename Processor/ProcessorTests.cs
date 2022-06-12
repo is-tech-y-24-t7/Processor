bf83c0e4125387feb5f2db90e9f2977509f85c9d
murovv <murovgleb@gmail.com>
2022-06-12 14:22:34 +0300

    added files to build directory

--- Processor/ProcessorTests.cs
+++ Processor/ProcessorTests.cs
@@ -11,7 +11,7 @@ public class ProcessorTests
     [Test]
     public void Test1()
     {
-        CpuMemorySimplified memory = new CpuMemorySimplified("../TestFiles/2plus2.txt");//A9 02 69 02 85 03
+        CpuMemorySimplified memory = new CpuMemorySimplified(AppDomain.CurrentDomain.BaseDirectory+"2plus2.txt");//A9 02 69 02 85 03
         Cpu cpu = new Cpu(memory);
         cpu.Step();
         cpu.Step();
@@ -23,7 +23,8 @@ public class ProcessorTests
     {
         //источник: https://www.lysator.liu.se/~nisse/misc/6502-mul.html#:~:text=Since%206502%20lacks%20any%20multiplication,if%20I%20count%20it%20correctly.
         //два множителя 10 и 10 записаны в ячейках 0x30 и 0x31
-        CpuMemorySimplified memory = new CpuMemorySimplified("../TestFiles/multiply.bin");
+        CpuMemorySimplified memory = new CpuMemorySimplified(AppDomain.CurrentDomain.BaseDirectory+"multiply.bin");
+        Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory+"multiply.bin");
         Cpu cpu = new Cpu(memory);
         for (int i = 0; i < 75; i++)
         {
